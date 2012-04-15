228353cacfb2bcf6654d3469115ca26beb908e6a
Gianpaulo Soares <jam_pow@hotmail.com>
2012-04-15 16:54:25 -0300

    substituindo tabs por espaço pra melhorar visualização no GITHUB

--- src/com/fiap/fundamentos/Bicicleta.java
+++ src/com/fiap/fundamentos/Bicicleta.java
@@ -4,23 +4,23 @@ import java.math.BigDecimal;
 
 public class Bicicleta {
 	//Definindo atributos
-	private String 		cor;
-	private int 		velocidade;
-	private int 		marcha;
-	private BigDecimal 	valor;
+	private String      cor;
+	private int         velocidade;
+	private int         marcha;
+	private BigDecimal  valor;
 
 	//get set para os atributos
-	public void 		setCor(String cor)		{ this.cor = cor; }
-	public String 		getCor()				{ return this.cor; }
+	public void         setCor(String cor)      { this.cor = cor; }
+	public String       getCor()                { return this.cor; }
 
-	public void 		setVelocidade(int vel)	{ this.velocidade = vel; }
-	public int 			getVelocidade()			{ return this.velocidade; }
+	public void         setVelocidade(int vel)  { this.velocidade = vel; }
+	public int          getVelocidade()         { return this.velocidade; }
 
-	public void 		setMarcha(int marcha)	{ this.marcha = marcha; }
-	public int 			getMarcha()				{ return this.marcha; }
+	public void         setMarcha(int marcha)   { this.marcha = marcha; }
+	public int          getMarcha()             { return this.marcha; }
 
-	public void 		setValor(double val)	{ this.valor = new BigDecimal(val); }
-	public BigDecimal 	getValor()				{ return this.valor; }
+	public void         setValor(double val)    { this.valor = new BigDecimal(val); }
+	public BigDecimal   getValor()              { return this.valor; }
 	
 	public static void main(String[] args) {
 		//nova instância de bicicleta em bic
